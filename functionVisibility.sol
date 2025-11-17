2ea4ffe1a9a186230b39bd91c2b27aa2e66efb69
shrestha-das <shresthadas.0.infinity@gmail.com>
2025-11-17 20:32:00 +0530

    public,private,internal,external

---
+++ functionVisibility.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+contract visibility {
+    // 1. public : can be used internally, externally
+
+    // 2. private : can be used within the contract
+
+    // 3. internal : can be used within the contract and other inherited contracts
+
+    // 4. external : can only be used in external contracts, accounts
+}
