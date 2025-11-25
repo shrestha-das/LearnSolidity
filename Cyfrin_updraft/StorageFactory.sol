5fc6daa54ee030a6f861926d1ff5e6642c4b1e4c
shrestha-das <shresthadas.0.infinity@gmail.com>
2025-11-25 23:06:54 +0530

    Example:SimpleFactory

--- Cyfrin_updraft/StorageFactory.sol
+++ Cyfrin_updraft/StorageFactory.sol
@@ -11,18 +11,18 @@ contract StorageFactory {
     SimpleStorage[] public ArrayOfSimpleStorages;
 
     function createSimpleStorageContract() public {
-        SimpleStorage newSimpleStorage = new SimpleStorage();
+        SimpleStorage newSimpleStorage = new SimpleStorage();  //creating a copy of SimpleStorage Contract
         ArrayOfSimpleStorages.push(newSimpleStorage);
     }
 
     function sfStore (uint256 _simpleStorageIndex, uint256 _favNumber) public {
-        SimpleStorage simpleStorage = ArrayOfSimpleStorages[_simpleStorageIndex];
-        simpleStorage.store(_favNumber);
+        SimpleStorage simpleStorage = ArrayOfSimpleStorages[_simpleStorageIndex];  // get the index
+        simpleStorage.store(_favNumber);  // store the favNo in that indexed Contract
     }
 
     function sfRetrieve (uint256 _simpleStorageIndex) public view returns(uint256) {
-        SimpleStorage simpleStorage = ArrayOfSimpleStorages[_simpleStorageIndex];
-        return simpleStorage.retrieve();
+        SimpleStorage simpleStorage = ArrayOfSimpleStorages[_simpleStorageIndex]; 
+        return simpleStorage.retrieve();  // retrieve the favNo
     }
 
 }
